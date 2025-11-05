d1a66f2ab5492e84b91a155d832bb3754fad4409
Sujit Jha <sujitjha@Sujit-Jha-Hakkoda-JQGL4GC2MX.local>
2025-11-05 11:28:54 +0530

    second commit

---
+++ 02_child_tables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE employees (
+    employee_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    department_id INT NOT NULL,
+    hire_date DATE NOT NULL,
+    salary DECIMAL(10,2),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+CREATE TABLE projects (
+    project_id SERIAL PRIMARY KEY,
+    project_name VARCHAR(100) NOT NULL,
+    department_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id),
+    FOREIGN KEY (manager_id) REFERENCES employees(employee_id)
+);
