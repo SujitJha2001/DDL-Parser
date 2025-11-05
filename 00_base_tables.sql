3df51dac48deb1f66432076e9f99a6c5f298badc
Sujit Jha <64594466+SujitJha2001@users.noreply.github.com>
2025-11-05 15:27:16 +0530

    Update 00_base_tables.sql

--- 00_base_tables.sql
+++ 00_base_tables.sql
@@ -1,8 +1,8 @@
-CREATE TABLE companies (
-    company_id SERIAL PRIMARY KEY,
-    company_name VARCHAR(100) NOT NULL,
-    founded_date DATE
-);
+-- CREATE TABLE companies (
+--     company_id SERIAL PRIMARY KEY,
+--     company_name VARCHAR(100) NOT NULL,
+--     founded_date DATE
+-- );
 
 CREATE TABLE locations (
     location_id SERIAL PRIMARY KEY,
