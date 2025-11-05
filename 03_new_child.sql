7f334320473f65c3711c14dc51e3a6dd509c9588
Sujit Jha <64594466+SujitJha2001@users.noreply.github.com>
2025-11-05 12:09:50 +0530

    Update and rename 03_new_tables.sql to 03_new_child.sql

---
+++ 03_new_child.sql
@@ -0,0 +1,8 @@
+CREATE TABLE project_members (
+    member_id SERIAL PRIMARY KEY,
+    project_id INT NOT NULL,
+    employee_id INT NOT NULL,
+    role VARCHAR(50),
+    FOREIGN KEY (project_id) REFERENCES projects(project_id),
+    FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
+);
--- 03_new_tables.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tasks (
-    task_id SERIAL PRIMARY KEY,
-    project_id INT NOT NULL,
-    assigned_to INT,
-    FOREIGN KEY (project_id) REFERENCES projects(project_id),
-    FOREIGN KEY (assigned_to) REFERENCES employees(employee_id)
-);
