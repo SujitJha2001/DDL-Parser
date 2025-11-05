29d38806312b8023d2cff71f3db18125c27b5a37
Sujit Jha <64594466+SujitJha2001@users.noreply.github.com>
2025-11-05 16:30:39 +0530

    Update 04_orphan.sql

--- 04_orphan.sql
+++ 04_orphan.sql
@@ -1,5 +1,5 @@
--- CREATE TABLE orphan_table (
---     id SERIAL PRIMARY KEY,
---     non_existent_parent_id INT NOT NULL,
---     FOREIGN KEY (non_existent_parent_id) REFERENCES non_existent_table(id)
--- );
+CREATE TABLE orphan_table (
+    id SERIAL PRIMARY KEY,
+    non_existent_parent_id INT NOT NULL,
+    FOREIGN KEY (non_existent_parent_id) REFERENCES non_existent_table(id)
+);
