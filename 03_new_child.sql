a0399826e1c07a01c0cfd5af4913cb97ffa65a19
Sujit Jha <64594466+SujitJha2001@users.noreply.github.com>
2025-11-05 13:50:58 +0530

    Update 03_new_child.sql

--- 03_new_child.sql
+++ 03_new_child.sql
@@ -3,6 +3,7 @@ CREATE TABLE project_members (
     project_id INT NOT NULL,
     employee_id INT NOT NULL,
     role VARCHAR(50),
+    -- EXPLICIT FOREIGN KEYS
     FOREIGN KEY (project_id) REFERENCES projects(project_id),
     FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
 );
