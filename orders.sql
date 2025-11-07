71ebe393660b4542a83c012e0a32dbcdc33a076e
Sujit Jha <64594466+SujitJha2001@users.noreply.github.com>
2025-11-07 13:01:30 +0530

    Delete orders.sql

--- orders.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE dbo.orders (
-    order_id INT PRIMARY KEY,
-    user_id INT NOT NULL REFERENCES users(user_id),  -- Inline reference
-    order_date DATETIME NOT NULL DEFAULT GETDATE(),
-    total_amount DECIMAL(10,2) NOT NULL
-);
