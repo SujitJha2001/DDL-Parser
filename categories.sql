97355b2c61bdd74911f0175e89b83373d8a796a3
Sujit Jha <64594466+SujitJha2001@users.noreply.github.com>
2025-11-07 12:48:25 +0530

    Update categories.sql

--- categories.sql
+++ categories.sql
@@ -1,1 +1,5 @@
-
+CREATE TABLE dbo.categories (
+    category_id INT PRIMARY KEY,
+    category_name VARCHAR(100) NOT NULL,
+    parent_category_id INT NULL REFERENCES categories(category_id)  -- Self-reference + inline
+);
