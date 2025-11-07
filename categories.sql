21202a7614f4d6d146b9d13fa4e76fadbb2a7d8c
Sujit Jha <64594466+SujitJha2001@users.noreply.github.com>
2025-11-07 13:01:46 +0530

    Delete categories.sql

--- categories.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE dbo.categories (
-    category_id INT PRIMARY KEY,
-    category_name VARCHAR(100) NOT NULL,
-    parent_category_id INT NULL REFERENCES categories(category_id)  -- Self-reference + inline
-);
