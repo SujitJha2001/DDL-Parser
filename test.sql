6c73e3031bcd8d4c57760dcc80bc3a02f8ec7e2d
Sujit Jha <64594466+SujitJha2001@users.noreply.github.com>
2025-11-11 13:57:57 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,11 +1,11 @@
 -- In schema Deloitte
-CREATE TABLE Deloitte.Client (
+CREATE OR REPLACE TABLE Deloitte.Client (
     ClientID INT PRIMARY KEY,
     ClientName VARCHAR(100)
 );
 
 -- In schema Sales  
-CREATE TABLE Sales.PurchOrders (
+CREATE OR REPLACE TABLE Sales.PurchOrders (
     OrderID INT PRIMARY KEY,
     ClientID INT REFERENCES Deloitte.Client(ClientID)  -- Cross-schema reference
 );
