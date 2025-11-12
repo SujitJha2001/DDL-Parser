3395831bc8601fd7a57073b883e01dad34d25ae6
Sujit Jha <64594466+SujitJha2001@users.noreply.github.com>
2025-11-12 11:05:11 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -9,3 +9,22 @@ CREATE OR REPLACE TABLE Sales.PurchOrders (
     OrderID INT PRIMARY KEY,
     ClientID INT REFERENCES Deloitte.Client(ClientID)  -- Cross-schema reference
 );
+
+CREATE OR REPLACE TABLE Deloitte.Deloitte_ClientList (
+    ClientEntityId INT NOT NULL,
+    ExternalAccountId NVARCHAR(255) NOT NULL,
+    LabelAccountId NVARCHAR(25) NOT NULL,
+    EffectiveFrom DATE NULL,
+    EffectiveTo DATE NULL,
+    AddedBy NVARCHAR(100) NOT NULL,
+    _Added_DateTime TIMESTAMP_NTZ(3) NOT NULL,
+    _Modified_DateTime TIMESTAMP_NTZ(3) NOT NULL,
+    _UpdateCounter INT DEFAULT ((0)) NOT NULL,
+    SysStartTime TIMESTAMP_NTZ(7) GENERATED ALWAYS AS ROW START HIDDEN !!!RESOLVE EWI!!! /*** SSC-EWI-0073 - PENDING FUNCTIONAL EQUIVALENCE REVIEW FOR 'GeneratedClause' NODE ***/!!! NOT NULL,
+    SysEndTime TIMESTAMP_NTZ(7) GENERATED ALWAYS AS ROW END HIDDEN !!!RESOLVE EWI!!! /*** SSC-EWI-0073 - PENDING FUNCTIONAL EQUIVALENCE REVIEW FOR 'GeneratedClause' NODE ***/!!!   NOT NULL,
+       CONSTRAINT PK_Deloitte_ClientList PRIMARY KEY (ClientEntityId, ExternalAccountId),
+       PERIOD FOR SYSTEM_TIME ([SysStartTime], [SysEndTime]) !!!RESOLVE EWI!!! /*** SSC-EWI-0073 - PENDING FUNCTIONAL EQUIVALENCE REVIEW FOR 'TablePeriodDefinition' NODE ***/!!!
+   )
+   WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE=[Deloitte].[Deloitte_ClientList_History], DATA_CONSISTENCY_CHECK=ON) !!!RESOLVE EWI!!! /*** SSC-EWI-0073 - PENDING FUNCTIONAL EQUIVALENCE REVIEW FOR 'TableOptionSystemVersioning' NODE ***/!!!)
+   COMMENT = '{ "origin": "sf_sc", "name": "snowconvert", "version": {  "major": 1,  "minor": 20,  "patch": "10.0" }, "attributes": {  "component": "transact",  "convertedOn": "11/11/2025",  "domain": "evelyn",  "migrationid": "tXGaAVYzjXO6zsbRGZv7Hg==" }}'
+;
