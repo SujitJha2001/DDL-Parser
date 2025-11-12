ef8d51fc8effd33a1531538df7943248a8fd8272
Sujit Jha <64594466+SujitJha2001@users.noreply.github.com>
2025-11-12 12:08:26 +0530

    Update 04_orphan.sql

--- 04_orphan.sql
+++ 04_orphan.sql
@@ -1,4 +1,4 @@
-CREATE TABLE orphan_table (
+CREATE TABLE ODS.Deloitte.orphan_table (
     id SERIAL PRIMARY KEY,
     non_existent_parent_id INT NOT NULL,
     FOREIGN KEY (non_existent_parent_id) REFERENCES non_existent_table(id)
