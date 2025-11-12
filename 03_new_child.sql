99ba3ccd4cfe351f802d87f4c36025a9223d9efc
Sujit Jha <64594466+SujitJha2001@users.noreply.github.com>
2025-11-12 12:05:30 +0530

    Delete 03_new_child.sql

--- 03_new_child.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE project_members (
-    member_id SERIAL PRIMARY KEY,
-    project_id INT NOT NULL,
-    employee_id INT NOT NULL,
-    role VARCHAR(50),
-    -- EXPLICIT FOREIGN KEYS
-    FOREIGN KEY (project_id) REFERENCES projects(project_id),
-    FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
-);
