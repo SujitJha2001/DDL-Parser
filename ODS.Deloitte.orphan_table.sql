4a9add4d6e5320e901085f43fe68e93a9472da71
Sujit Jha <64594466+SujitJha2001@users.noreply.github.com>
2025-11-12 16:28:27 +0530

    Delete ODS.Deloitte.orphan_table.sql

--- ODS.Deloitte.orphan_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE ODS.Deloitte.orphan_table (
-    id SERIAL PRIMARY KEY,
-    non_existent_parent_id INT NOT NULL,
-    FOREIGN KEY (non_existent_parent_id) REFERENCES ODS.Deloitte.non_existent_table(id)
-);
